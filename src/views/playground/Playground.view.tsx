132fb7421d7edb229fd666bd935998b83e68ab49
burcutosun <124238456+burcutosun@users.noreply.github.com>
2024-08-07 13:54:12 +0300

    Fix

--- src/views/playground/Playground.view.tsx
+++ src/views/playground/Playground.view.tsx
@@ -85,13 +85,13 @@ export function Component() {
                       className='h-6 w-6 text-white'
                       fill='none'
                       viewBox='0 0 24 24'
-                      stroke-width='1.5'
+                      strokeWidth='1.5'
                       stroke='currentColor'
                       aria-hidden='true'
                     >
                       <path
-                        stroke-linecap='round'
-                        stroke-linejoin='round'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
                         d='M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z'
                       />
                     </svg>
@@ -110,13 +110,13 @@ export function Component() {
                       className='h-6 w-6 text-white'
                       fill='none'
                       viewBox='0 0 24 24'
-                      stroke-width='1.5'
+                      strokeWidth='1.5'
                       stroke='currentColor'
                       aria-hidden='true'
                     >
                       <path
-                        stroke-linecap='round'
-                        stroke-linejoin='round'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
                         d='M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z'
                       />
                     </svg>
@@ -135,13 +135,13 @@ export function Component() {
                       className='h-6 w-6 text-white'
                       fill='none'
                       viewBox='0 0 24 24'
-                      stroke-width='1.5'
+                      strokeWidth='1.5'
                       stroke='currentColor'
                       aria-hidden='true'
                     >
                       <path
-                        stroke-linecap='round'
-                        stroke-linejoin='round'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
                         d='M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99'
                       />
                     </svg>
@@ -160,13 +160,13 @@ export function Component() {
                       className='h-6 w-6 text-white'
                       fill='none'
                       viewBox='0 0 24 24'
-                      stroke-width='1.5'
+                      strokeWidth='1.5'
                       stroke='currentColor'
                       aria-hidden='true'
                     >
                       <path
-                        stroke-linecap='round'
-                        stroke-linejoin='round'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
                         d='M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33'
                       />
                     </svg>
@@ -195,12 +195,12 @@ export function Component() {
                 cy='512'
                 r='512'
                 fill='url(#759c1415-0410-454c-8f7c-9a820de03641)'
-                fill-opacity='0.7'
+                fillOpacity='0.7'
               />
               <defs>
                 <radialGradient id='759c1415-0410-454c-8f7c-9a820de03641'>
                   <stop stop-color='#7775D6' />
-                  <stop offset='1' stop-color='#E935C1' />
+                  <stop offset='1' stopColor='#E935C1' />
                 </radialGradient>
               </defs>
             </svg>
@@ -276,9 +276,9 @@ export function Component() {
                     aria-hidden='true'
                   >
                     <path
-                      fill-rule='evenodd'
+                      fillRule='evenodd'
                       d='M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z'
-                      clip-rule='evenodd'
+                      clipRule='evenodd'
                     />
                   </svg>
                   Private forum access
@@ -291,9 +291,9 @@ export function Component() {
                     aria-hidden='true'
                   >
                     <path
-                      fill-rule='evenodd'
+                      fillRule='evenodd'
                       d='M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z'
-                      clip-rule='evenodd'
+                      clipRule='evenodd'
                     />
                   </svg>
                   Member resources
@@ -306,9 +306,9 @@ export function Component() {
                     aria-hidden='true'
                   >
                     <path
-                      fill-rule='evenodd'
+                      fillRule='evenodd'
                       d='M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z'
-                      clip-rule='evenodd'
+                      clipRule='evenodd'
                     />
                   </svg>
                   Entry to annual conference
@@ -321,9 +321,9 @@ export function Component() {
                     aria-hidden='true'
                   >
                     <path
-                      fill-rule='evenodd'
+                      fillRule='evenodd'
                       d='M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z'
-                      clip-rule='evenodd'
+                      clipRule='evenodd'
                     />
                   </svg>
                   Official member t-shirt
